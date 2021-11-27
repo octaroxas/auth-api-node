4c363feefe6b509b63f718e61144354eb9b7e7a8
Octacilio C. de Almeida <octa.oca44@gmail.com>
2021-11-27 19:33:12 -0300

    Servidor configurado com ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Projeto rodando')
+import express, { Request, Response, NextFunction}  from 'express'
+
+const PORT = 3333;
+
+const app = express()
+
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({msg: 'ok'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running! in http://localhost:${PORT}`)
+})
