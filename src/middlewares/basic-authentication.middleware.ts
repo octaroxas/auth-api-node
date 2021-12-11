80045f4c503d4f2e9068c5fa1c00b7202864544f
Octacilio C. Ameida <octa.oca44@gmail.com>
2021-12-11 13:39:30 -0300

    Basic auth implementada

---
+++ src/middlewares/basic-authentication.middleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import ForbiddenError from "../models/errors/forbidden.error.model";
+import userRepository from "../repositories/user.repository";
+
+export default async function basicAuthenticationMiddleware(req: Request, res: Response, next: NextFunction) {
+
+    try {
+
+        const authHeader = req.headers['authorization'];
+        
+        if(!authHeader) {
+            throw new ForbiddenError("Credenciais não informadas!")
+        } 
+
+        const [authType, token] = authHeader.split(' ');
+
+        if(authType !== 'Basic' || !token ) {
+            throw new ForbiddenError('Tipo de autenticação inválida!')
+        }
+
+        // o token base64 é desemcriptado e armazenado em tokenContent como uma string
+        const tokenContent = Buffer.from(token,'base64').toString('utf-8')
+
+        // tokenContent = username:password
+        const [username, password] = tokenContent.split(':')
+
+        if(!username || !password) {
+            throw new ForbiddenError("Credenciais não informadas");
+        }
+
+        const user = await userRepository.getUserByCredentials(username, password)
+        
+        if(!user) {
+            throw new ForbiddenError('Usuario ou senha incorretos!')
+        }
+
+        req.user = user;
+        next()
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,41 +3,17 @@ import ForbiddenError from "../models/errors/forbidden.error.model";
 import userRepository from "../repositories/user.repository";
 import JWT from 'jsonwebtoken'
 import { StatusCodes } from "http-status-codes";
+import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
 
 const auth = Router();
 
-auth.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+auth.post('/token',basicAuthenticationMiddleware ,async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const authHeader = req.headers['authorization'];
-        
-        if(!authHeader) {
-            throw new ForbiddenError("Credenciais não informadas!")
-        } 
 
-        const [authType, token] = authHeader.split(' ');
+        const user = req.user;
 
-        if(authType !== 'Basic' || !token ) {
-            throw new ForbiddenError('Tipo de autenticação inválida!')
-        }
-
-        // o token base64 é desemcriptado e armazenado em tokenContent como uma string
-        const tokenContent = Buffer.from(token,'base64').toString('utf-8')
-
-        // tokenContent = username:password
-        const [username, password] = tokenContent.split(':')
-
-        if(!username || !password) {
-            throw new ForbiddenError("Credenciais não informadas");
-        }
-
-        const user = await userRepository.getUserByCredentials(username, password)
-        
-        if(!user) {
-            throw new ForbiddenError('Usuario ou senha incorretos!')
-        }
-
-        const payload = {username: user.username}
+        const payload = {username: user?.username}
         const options =  {subject: user?.uuid}
         
         const jwt = JWT.sign(payload,'secret-hash',options)
